7ac083fcdc25635526885ada8f66d87a1aaabb8d
Lucas Sanders <git@butterflyhug.com>
2022-07-04 16:35:42 -0400

    More UX and JavaScript API polish
    - More consistent headings
    - If the file upload input has a value on initial page load, use it
    - Convert javascript: hrefs to event handlers
    - Tweak min/max image sizes to powers of two
    - When framing low-res images, don't render the canvas larger than
      its intrinsic size
    - Underline the text for the active frame style's button
    - Consistently use single quotes in JS and rem sizes in CSS

--- src/script.js
+++ src/script.js
@@ -1,22 +1,27 @@
 (() => {
-  const context = document.querySelector("canvas").getContext("2d");
+  const uploadedImage = new Image();
+  uploadedImage.src = 'placeholder.svg';
 
-  function drawCanvas() {
-    if (!uploadedImage.src) return;
+  const frameImage = new Image();
+  frameImage.src = '';
+  frameImage.dataset.innerToOuterRatio = '1.0';
 
+  function drawCanvas(frameInnerToOuterRatio) {
+    const context = document.querySelector('canvas').getContext('2d');
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     context.moveTo(0, 0);
     context.beginPath();
 
+    const innerToOuterRatio = +frameImage.dataset.innerToOuterRatio;
     const {width, height} = uploadedImage;
     const minDimension = Math.min(width, height);
 
-    const sizeUnclamped = Math.floor(minDimension / frameInnerToOuterRatio / 2) * 2;
+    const sizeUnclamped = Math.floor(minDimension / innerToOuterRatio / 2) * 2;
     // Try to set things up so the image in the middle retains its original
     // resolution. But don't let the frame resolution get too low, or the final
     // resolution get too large. (Firefox in particular doesn't downscale well,
     // so results are poorer.)
-    const size = Math.min(Math.max(250, sizeUnclamped), 800);
+    const size = Math.min(Math.max(256, sizeUnclamped), 1024);
     context.canvas.width = size;
     context.canvas.height = size;
 
@@ -45,9 +50,21 @@
     context.drawImage(frameImage, 0, 0, size, size);
 
     const canvas = document.querySelector('canvas');
-    const exportLink = document.querySelector('#export');
+    const exportLink = document.querySelector('#export-link');
     exportLink.href = canvas.toDataURL('image/png');
+
+    for (const link of document.querySelectorAll('a.button')) {
+      if (frameImage.src.includes(link.dataset.src)) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
+      }
+    }
   }
+
+  uploadedImage.addEventListener('load', drawCanvas);
+  frameImage.addEventListener('load', drawCanvas);
+
 
   function readImage() {
     if (!this.files || !this.files[0]) return;
@@ -58,24 +75,18 @@
     });
     reader.readAsDataURL(this.files[0]);
   }
-  document.querySelector("input[type='file']")
-      .addEventListener("change", readImage);
-
-  function setBorder(innerToOuterRatio, filename) {
-    frameImage.src = filename;
-    frameInnerToOuterRatio = innerToOuterRatio;
+  const fileUploadInput = document.querySelector('input[type="file"]');
+  fileUploadInput.addEventListener('change', readImage);
+  if (fileUploadInput.files && fileUploadInput.files.length) {
+    readImage.call(fileUploadInput);
   }
 
-  const uploadedImage = new Image();
-  uploadedImage.src = 'placeholder.svg';
-  uploadedImage.addEventListener("load", drawCanvas);
 
-  const frameImage = new Image();
-  frameImage.src = '';
-  frameImage.addEventListener("load", drawCanvas);
-  // Ratio between the inner size of the frame (where the uploaded image gets
-  // drawn) to the outer size of the frame.
-  let frameInnerToOuterRatio = 0;
+  function setBorder(linkElement) {
+    frameImage.dataset.innerToOuterRatio = linkElement.dataset.visibleRatio;
+    frameImage.src = linkElement.dataset.src;
+  }
+  window.setBorder = setBorder;
 
   // Trigger the default frame to set the defaults for the above.
   document.querySelector('#frame-list-default').click();
